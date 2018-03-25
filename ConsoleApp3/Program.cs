ab7323a155ea3bac32e36486d575677805a37fd7
Valtaya <37625674+Valtaya@users.noreply.github.com>
2018-03-25 14:23:42 +0100

    Assignment7
    added comments

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -138,20 +138,25 @@ namespace Assignment6
         }
         public static int Average()
         {
+            //set variable
             int counter = 0;
+            //establish connection:
             conn = new OleDbConnection();
             conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\nicol\Documents\Vehicles.accdb";
             cmd = new OleDbCommand();
             cmd.Connection = conn;
+            //command:
             cmd.CommandText = "SELECT * FROM Vehicles";
             conn.Open();
             reader = cmd.ExecuteReader();
             while (reader.Read())
             {
+                //increment counter
                 counter++;
             }
-
+            //close connection:
             conn.Close();
+            //print result:
             Console.WriteLine("====================================");
             Console.WriteLine("Number of Permits issued :" + counter);
             Console.WriteLine("====================================");
@@ -172,63 +177,80 @@ namespace Assignment6
 
         public static void Unique()
         {
+            //establish connection:
             conn = new OleDbConnection();
             conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\nicol\Documents\Vehicles.accdb";
             cmd = new OleDbCommand();
             cmd.Connection = conn;
+            //command:
             cmd.CommandText = "SELECT IDNumber, Vehicle_Model, Vehicle_Model2, Vehicle_Model3 FROM Vehicles";
             conn.Open();
 
             reader = cmd.ExecuteReader();
+            //while reader reads:
             while (reader.Read())
             {
+                //counter to track number of vehicles:
                 int counter = 0;
+                //cycle through vehicles:
                 for (int i = 1; i <= 3; i++)
                 {
+                    //if entry exists:
                     if (reader[i] != DBNull.Value)
                     {
+                        //increment:
                         counter++;
+                        //if cell is empty or null:
                         if (String.IsNullOrEmpty(Convert.ToString(reader[i])))
                         {
+                            //decrement:
                             counter--;
                         }
                     }
                 }
+                //display result:
                 Console.WriteLine(reader[0] + " has " + counter + " unique vehicles assigned to permit.");
             }
+            //close connection:
             conn.Close();
         }
 
-        /*public static void Fees()
-        {
-
-        }*/
         public static int FeesCalc()
         {
-
+            //establish connection:
             conn = new OleDbConnection();
             conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\nicol\Documents\Vehicles.accdb";
             cmd = new OleDbCommand();
             cmd.Connection = conn;
+            //set variables:
             int fees = 80;
             int feesTotal = 0;
+            //command:
             cmd.CommandText = "SELECT * FROM Vehicles";
+            //open connection:
             conn.Open();  
             reader = cmd.ExecuteReader();
+            //while reader reads:
             while (reader.Read())
             {
-                //store expiration date 
+                //expiry date :
                 DateTime expire = Convert.ToDateTime(reader["Expiring"]);
-                int due = 0;    //fees due on current permit
-                if (expire < DateTime.Now)  //if current date later than expiry 
+                //fees due:
+                int due = 0;
+                //if date is past expiry date:
+                if (expire < DateTime.Now) 
                 {
-                    feesTotal = feesTotal + fees;   //add to total fees
-                    due += fees;                    //add to current permit fees 
+                    //add fees to total:
+                    feesTotal = feesTotal + fees;
+                    //add to total due:
+                    due += fees;
                 }
+                //print amount due per person:
                 Console.WriteLine(due + " euro on permit: " + reader[0]);
             }
+            //print total due:
             Console.WriteLine("Total fees due: " + feesTotal);
-            //close the reader when no longer needed
+            //close reader:
             reader.Close();
             return feesTotal;
         }
